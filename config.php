f0161022db970619c278c28f4e64c9a2dc00011f
Douglas Jasper <doug.jasper@trios.com>
2025-06-17 09:14:57 -0400

    DB Class Created

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php 
+//Stripe Credentials Configuration
+define("STRIPE_SECRET_API_KEY", "YOUR STRIPE SECRET KEY");
+define("STRIPE_PUBLISHABLE_KEY", "YOUR STRIPE PUBLISHABLE KEY");
+
+//Sample Product Details
+define('CURRENCY', 'USD');
+define('AMOUNT', '10');
+define('DESCRIPTION', 'Laptop Bag');
+
+// Database Credentials Configuration 
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'allphptricks_cart');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+?>
